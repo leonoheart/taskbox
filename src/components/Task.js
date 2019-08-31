2540bf564dd9cecd23504f882998c93cd11682a5
Emma <emma@MacBook-Pro-2.local>
2019-08-31 20:17:25 +0900

    fix the snapshots test error

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,22 +1,48 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 
 export default class Task extends Component {
   render() {
+    const task = this.props.task
+    const onPinTask = this.props.onPinTask
+    const onArchiveTask = this.props.onArchiveTask
+
     return (
-      <div className={`list-item ${this.props.task.state}`}>
+      <div className={`list-item ${task.state}`}>
         <label className="checkbox">
           <input 
           type="checkbox"
-          defaultChecked={this.props.task.state === 'TASK_ARCHIVED'}
+          defaultChecked={task.state === 'TASK_ARCHIVED'}
           disabled={true}
           name="checked"
           />
-          <span className="checkbox-custom" onClick={() => this.props.actions.onArchiveTask(this.props.task.id)}></span>
+          <span className="checkbox-custom" onClick={() => onArchiveTask(task.id)}></span>
         </label>
-        {/* <div className="title">
-          <input type="text" value={this.props.task.title} readOnly={true} placeholder="Input title"/>  
-        </div> */}
+
+        <div className="title">
+          <input type="text" value={task.title} readOnly={true} placeholder="Input title"/>  
+        </div>
+
+        <div className="actions" onClick={event => event.stopPropagation()}>
+          {
+            task.state !== 'TASK_ARCHIVED' && (
+              <a onClick={() => onPinTask(task.id)}>
+                <span className={`icon-star`}></span>
+              </a>
+            )
+          }
+        </div>
       </div>
     )
   } 
+}
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state:PropTypes.string.isRequired
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func
 }
